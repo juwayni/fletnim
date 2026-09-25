b829d20fb361b08c673a95895cd1ab9ae1a382b7
juwayni <ghazalialshafi@outlook.com>
2026-09-25 20:39:22 +0530

    Delete .github directory

--- .github/scripts/templates/cupertino_icons.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-List<IconData> cupertinoIcons = [
-  {% for name, code in icons -%}
-  CupertinoIcons.{{ name }},
-  {% endfor -%}
-];
--- .github/scripts/templates/material_icons.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-List<IconData> materialIcons = [
-  {% for name, code in icons -%}
-  Icons.{{ name }},
-  {% endfor -%}
-];
